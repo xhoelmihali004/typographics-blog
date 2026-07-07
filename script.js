84cd7c7898f5e9d16e6d5a602ce04f144bf4d7ad
xhoelmihali004 <xhoelmihali04@gmail.com>
2026-07-07 15:27:13 +0200

    added scss styling

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ setTimeout(() => {
     document.querySelectorAll('.hidden').forEach(el => el.style.display = 'block');
 }, 3000);
 
-
-
-
-
 // Function to check if page is scrolled and adjust the logo size
 function checkScroll() {
     const navbar = document.getElementById('navbar');
@@ -33,3 +29,52 @@ function checkScroll() {
 
 // Event Listener for scroll event
 window.addEventListener('scroll',checkScroll);
+
+// Dark Mode------------------------------------------------------------------------------
+const themeSwitcher = document.getElementById('theme-switcher');
+
+function updateThemeIcon(isDarkMode) {
+    themeSwitcher.children[0].classList.replace(isDarkMode ? 'fa-sun':'fa-moon', isDarkMode ? 'fa-moon' : 'fa-sun')
+}
+
+// Determine if dark mode is preferred
+function prefersDarkMode() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+// Set the theme based on the preference
+function setThemeBasedOnPreference() {
+    const isDarkMode = prefersDarkMode();
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+    updateThemeIcon(isDarkMode);
+}
+
+// Switch Theme manually
+function switchTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    updateThemeIcon(newTheme === 'dark');
+}
+
+// Event Listener
+themeSwitcher.addEventListener('click', switchTheme);
+
+// Initialize Theme
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+        updateThemeIcon(savedTheme === 'dark');
+    } else {
+        setThemeBasedOnPreference();
+    }
+}
+
+// Listen for system theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeBasedOnPreference);
+
+// Initialize theme when the script loads
+initializeTheme();
